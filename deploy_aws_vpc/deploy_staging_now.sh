d6ab1f22c2d0ad2801f6bc87ed56440d8fc064a3
Mark Scrimshire <mscrimshire+github@gmail.com>
2019-08-12 19:54:04 -0400

    update deploy_staging_now.sh

--- deploy_aws_vpc/deploy_staging_now.sh
+++ deploy_aws_vpc/deploy_staging_now.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /mgmt-ctrl/Infrastructure/deploy_aws_vpc
-ansible-playbook -i ./etc/ansible/hosts  ./vpc.yml --vault-password-file ../env/ansibled.vault --extra-vars "vpc_env=staging vpc_base_region=us-east vpc_number=1 aws_region=us-east-1"  -v
+ansible-playbook -i ./etc/ansible/hosts  ./vpc.yml --vault-password-file ../env/ansibled.vault --extra-vars "vpc_env=staging vpc_base_region=us-east vpc_number=1 aws_region=us-east-1 ansible_python_interpreter=/mgmt-ctrl/cdex-deploy/env/bin/python3"  -v
 echo "if there are problems running with python3 add ansible_python_interpreter={location of python3}/python3 to extra-vars"
 echo "Now go to AWS Console in US-EAST-1 (N.Virginia)"
 echo "Add new servers to PROD-WEB target group"
